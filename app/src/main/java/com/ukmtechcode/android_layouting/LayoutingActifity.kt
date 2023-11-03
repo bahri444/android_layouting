feca509eff95846e81efcacc668ecbe3b92c5c2f
bahri444 <bahrysaipul266@gmail.com>
2023-11-03 16:33:10 +0700

    update to view binding

--- app/src/main/java/com/ukmtechcode/android_layouting/LayoutingActifity.kt
+++ app/src/main/java/com/ukmtechcode/android_layouting/LayoutingActifity.kt
@@ -4,38 +4,27 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
+import com.ukmtechcode.android_layouting.databinding.ActivityLayoutingBinding
 
 class LayoutingActifity : AppCompatActivity() {
-    private lateinit var getPanjang :TextView
-    private lateinit var getLebar :TextView
-    private lateinit var getTextHitung:TextView
-    private lateinit var getTombolHitung:Button
-    private lateinit var getTinggi:TextView
+//    deklarasi view binding
+    private lateinit var binding: ActivityLayoutingBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_layouting)
-
-        //call funtion initComponent
-        initComponent()
-        getTombolHitung.setOnClickListener {
-            val panjang = getPanjang.text.toString()
-            val lebar = getLebar.text.toString()
-            val tinggi = getTinggi.text.toString()
+        binding= ActivityLayoutingBinding.inflate(layoutInflater)
+        setContentView(binding.root)  //dapat menggantikan pemanggilan view xml
+        binding.tombolHitung.setOnClickListener {
+            val panjang = binding.inputPanjang.text.toString()
+            val lebar = binding.inputLebar.text.toString()
+            val tinggi = binding.inputTinggi.text.toString()
 
 //            check apakah inputan nilai ada nilai yang di inputkan atau tidak
             if (lebar.isEmpty() || panjang.isEmpty() || tinggi.isEmpty()){
-                getTextHitung.text="inputan tidak boleh kosong"
+                binding.textCount.text="inputan tidak boleh kosong"
             }else{
                 val hasil = panjang.toInt() * lebar.toInt() * tinggi.toInt()
-                getTextHitung.text = hasil.toString()
+                binding.textCount.text = hasil.toString()
             }
         }
-    }
-    private fun initComponent(){
-        getPanjang = findViewById(R.id.inputPanjang)
-        getLebar = findViewById(R.id.inputLebar)
-        getTextHitung = findViewById(R.id.text_count)
-        getTombolHitung = findViewById(R.id.tombol_hitung)
-        getTinggi = findViewById(R.id.inputTinggi)
     }
 }
